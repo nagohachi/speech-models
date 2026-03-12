fc55a4afedd23cd7f7c7ed39298778c8a9c28f44
nagohachi <nagohachi.study@gmail.com>
2026-03-12 13:01:58 +0900

    added specaug during training

--- speech_models/modules/frontend/log_mel.py
+++ speech_models/modules/frontend/log_mel.py
@@ -8,8 +8,12 @@ class BatchedFbank(nn.Module):
         self,
         sample_rate: int = 16000,
         n_mels: int = 80,
-        frame_length: float = 25.0,  # ms
-        frame_shift: float = 10.0,  # ms
+        frame_length: float = 25.0,
+        frame_shift: float = 10.0,
+        apply_specaug: bool = True,
+        specaug_freq_mask_param: int = 27,
+        specaug_time_mask_param: int = 10000,
+        specaug_time_prob: float = 0.05,
     ) -> None:
         super().__init__()
 
@@ -28,25 +32,31 @@ class BatchedFbank(nn.Module):
         )
         self.eps = 1e-6
 
+        self.freq_mask = torchaudio.transforms.FrequencyMasking(
+            freq_mask_param=specaug_freq_mask_param, iid_masks=True
+        )
+
+        self.time_mask = torchaudio.transforms.TimeMasking(
+            time_mask_param=specaug_time_mask_param, iid_masks=True, p=specaug_time_prob
+        )
+
+        self.apply_specaug = apply_specaug
+
     def forward(
         self, wavs: torch.Tensor, wav_lens: torch.Tensor
     ) -> tuple[torch.Tensor, torch.Tensor]:
-        """
-        Args:
-            wavs (torch.Tensor): 音声波形 (batch_size, max_num_samples)
-            wav_lens (torch.Tensor): 各音声の実際のサンプル数 (batch_size,)
 
-        Returns:
-            tuple[torch.Tensor, torch.Tensor]:
-                - mel spectrogram (batch_size, num_frames, num_mel_bins)
-                - frame length (batch_size,)
-        """
         melspec = self.mel_spectrogram(wavs)
-
         log_melspec = torch.log(melspec + self.eps)
 
-        log_melspec = log_melspec.transpose(1, 2)
+        if self.apply_specaug and self.training:
+            log_melspec = self.freq_mask(log_melspec)
+            log_melspec = self.freq_mask(log_melspec)
 
+            for _ in range(10):
+                log_melspec = self.time_mask(log_melspec)
+
+        log_melspec = log_melspec.transpose(1, 2)
         xlens = wav_lens // self.mel_spectrogram.hop_length + 1
 
         return log_melspec, xlens
