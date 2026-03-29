ad2ac52fdb9bb3fc18f9731291e2cd3960dc40f6
nagohachi <nagohachi.study@gmail.com>
2026-03-29 21:36:35 +0900

    modified transformer encoder

--- speech_models/modules/encoder/transformer/transformer_encoder.py
+++ speech_models/modules/encoder/transformer/transformer_encoder.py
@@ -6,6 +6,7 @@ from speech_models.modules.utils.mask import lens_to_mask
 class TransformerEncoder(nn.Module):
     def __init__(self, hidden_size: int, num_heads: int, num_layers: int):
         super().__init__()
+        self.hidden_size = hidden_size
         encoder_layer = nn.TransformerEncoderLayer(d_model=hidden_size, nhead=num_heads)
         self.transformer_encoder = nn.TransformerEncoder(
             encoder_layer, num_layers=num_layers
